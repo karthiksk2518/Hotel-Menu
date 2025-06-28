bc09ca5002387e91e7328e1935cabf3915539cf2
Kundan Patidar <karthikpatidar2518@gmail.com>
2025-06-28 23:11:35 +0530

    Update done

--- Hotel_Menu_Backend/server.js
+++ Hotel_Menu_Backend/server.js
@@ -20,5 +20,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+app.get("/", (req,res) => {
+    res.send("API Working");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
