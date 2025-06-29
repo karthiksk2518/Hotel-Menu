e6fccb60f684c0fca0a85230052c72465ed26b2f
Kundan Patidar <karthikpatidar2518@gmail.com>
2025-06-29 16:40:41 +0530

    spinner color

--- Hotel_Menu_Frontend/src/pages/Home.jsx
+++ Hotel_Menu_Frontend/src/pages/Home.jsx
@@ -51,7 +51,7 @@ const Home = () => {
   if (loading) {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="w-16 h-16 border-8 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        <div className="w-16 h-16 border-8 border-emerald-500 border-t-transparent rounded-full animate-spin"></div>
         <p className="mt-4 text-lg text-gray-600">
           Wait a moment while Loading...
         </p>
